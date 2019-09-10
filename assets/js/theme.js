3435fe900494f4ce42f6f382b7383140b587e95c
Alfian Imanuddin <37921584+alfianimanuddin@users.noreply.github.com>
2019-09-10 14:07:51 +0700

    initial commit

---
+++ assets/js/theme.js
@@ -0,0 +1,12 @@
+var PSV = new PhotoSphereViewer({
+    panorama: 'https://cdn.rawgit.com/mistic100/Photo-Sphere-Viewer/3.1.0/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
+    container: 'photosphere',
+    loading_img: 'https://loading.io/spinners/double-ring/lg.double-ring-spinner.gif',
+    navbar: 'autorotate zoom fullscreen',
+    default_fov: 100,
+    mousewheel: true,
+    time_anim: false
+    // size: {
+    //   height: 400
+    // }
+});
